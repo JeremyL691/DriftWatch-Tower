746c56202f1b529b9352a00345a5b39d174651f1
Jeremy Liu <jiuyiliu@berkeley.edu>
2026-05-26 00:02:37 +0800

    Keep Testcontainers alive across integration tests

--- src/test/java/com/driftwatch/support/ContainerIntegrationTest.java
+++ src/test/java/com/driftwatch/support/ContainerIntegrationTest.java
@@ -5,30 +5,40 @@ import com.driftwatch.persistence.QualityAlertRepository;
 import com.driftwatch.persistence.RawEventRepository;
 import com.driftwatch.persistence.SchemaVersionRepository;
 import com.driftwatch.persistence.SourceHealthRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
+
+import java.util.stream.Stream;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Testcontainers(disabledWithoutDocker = true)
 public abstract class ContainerIntegrationTest {
 
-    @Container
     static final PostgreSQLContainer<?> POSTGRES =
             new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"));
 
-    @Container
     static final KafkaContainer KAFKA =
             new KafkaContainer(DockerImageName.parse("apache/kafka:3.8.0"));
+
+    private static final boolean DOCKER_AVAILABLE = DockerClientFactory.instance().isDockerAvailable();
+
+    static {
+        if (DOCKER_AVAILABLE) {
+            Startables.deepStart(Stream.of(POSTGRES, KAFKA)).join();
+        }
+    }
 
     @Autowired protected RawEventRepository rawEventRepository;
     @Autowired protected QualityAlertRepository qualityAlertRepository;
@@ -36,8 +46,18 @@ public abstract class ContainerIntegrationTest {
     @Autowired protected MetricWindowRepository metricWindowRepository;
     @Autowired protected SourceHealthRepository sourceHealthRepository;
 
+    @BeforeAll
+    static void ensureDockerIsAvailable() {
+        org.junit.jupiter.api.Assumptions.assumeTrue(
+                DOCKER_AVAILABLE,
+                "Docker is required for container-backed integration tests");
+    }
+
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
+        if (!DOCKER_AVAILABLE) {
+            return;
+        }
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
